e237e692ab075e75a80d798eef0514cbcb8db4ac
René El Fatuaki <rene@elfatuaki.com>
2024-06-12 12:37:51 +0200

    Added ProductNotFoundException when product is not found.

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -7,16 +7,13 @@ public class ShopService {
     private final ProductRepo productRepo = new ProductRepo();
     private final OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductNotFoundException {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             Optional<Product> productToOrder = productRepo.getProductById(productId);
-            if (productToOrder.isEmpty()) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            } else {
-                products.add(productToOrder.get());
-            }
+
+            products.add(productToOrder.orElseThrow(() -> new ProductNotFoundException("Product not found!")));
+
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
